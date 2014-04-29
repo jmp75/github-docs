ee63d3d59a10253a191780e232d3b006f0d7616f
J-M <jean-michel.perraud@csiro.au>
2014-04-29 10:04:43 +1000

    Jekyll notes

--- rClr/docs/tools/generate.fsx
+++ rClr/docs/tools/generate.fsx
@@ -6,40 +6,40 @@
 // Binaries that have XML documentation (in a corresponding generated XML file)
 let referenceBinaries = [ "Deedle.dll" ]
 // Web site location for the generated documentation
-let website = "http://bluemountaincapital.github.io/Deedle"
+let website = "http://jmp75.github.io/rClr"
 
 // Specify more information about your project
 let info =
-  [ "project-name", "Deedle"
-    "project-author", "BlueMountain Capital"
-    "project-summary", "Easy to use .NET library for data manipulation and scientific programming"
-    "project-github", "http://github.com/BlueMountainCapital/Deedle"
-    "project-nuget", "https://nuget.org/packages/Deedle" ]
+  [ "project-name", "rClr"
+    "project-author", "Jean-Michel Perraud"
+    "project-summary", "Access arbitrary .NET code from R in-process"
+    "project-github", "http://github.com/jmp75/rClr"
+    "project-nuget", "https://notavailable/packages" ]
 
 // --------------------------------------------------------------------------------------
 // For typical project, no changes are needed below
 // --------------------------------------------------------------------------------------
 
-#I "../../packages/FSharp.Formatting.2.2.12-beta/lib/net40"
-#I "../../packages/RazorEngine.3.3.0/lib/net40/"
-#r "../../packages/Microsoft.AspNet.Razor.2.0.30506.0/lib/net40/System.Web.Razor.dll"
-#r "../../packages/FAKE/tools/FakeLib.dll"
+//#I "../deps/packages/FSharp.Formatting.2.2.12-beta/lib/net40"
+#I "../deps/packages/RazorEngine.3.4.1/lib/net45/"
+#r "../deps/packages/Microsoft.AspNet.Razor.3.1.0/lib/net45/System.Web.Razor.dll"
+#r "../deps/packages/FAKE.2.6.12.0/tools/FakeLib.dll"
 #r "RazorEngine.dll"
-#r "FSharp.Literate.dll"
-#r "FSharp.CodeFormat.dll"
-#r "FSharp.MetadataFormat.dll"
+//#r "FSharp.Literate.dll"
+//#r "FSharp.CodeFormat.dll"
+//#r "FSharp.MetadataFormat.dll"
 open Fake
 open System.IO
 open Fake.FileHelper
-open FSharp.Literate
-open FSharp.MetadataFormat
+//open FSharp.Literate
+//open FSharp.MetadataFormat
 
 // When called from 'build.fsx', use the public project URL as <root>
 // otherwise, use the current 'output' directory.
 #if RELEASE
 let root = website
 #else
-let root = "file://" + (__SOURCE_DIRECTORY__ @@ "../output")
+let root = "file://" + (__SOURCE_DIRECTORY__ @@ "./output")
 #endif
 
 // Paths with template/source/output locations
@@ -48,7 +48,7 @@ let content    = __SOURCE_DIRECTORY__ @@ "../content"
 let output     = __SOURCE_DIRECTORY__ @@ "../output"
 let files      = __SOURCE_DIRECTORY__ @@ "../files"
 let templates  = __SOURCE_DIRECTORY__ @@ "templates"
-let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting.2.2.12-beta/"
+let formatting = __SOURCE_DIRECTORY__ @@ "../deps/packages/FSharp.Formatting.2.2.12-beta/"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.csproj templates (in this order)
@@ -64,12 +64,12 @@ let copyFiles () =
     |> Log "Copying styles and scripts: "
 
 // Build API reference from XML comments
-let buildReference () =
-  CleanDir (output @@ "reference")
-  for lib in referenceBinaries do
-    MetadataFormat.Generate
-      ( bin @@ lib, output @@ "reference", layoutRoots, 
-        parameters = ("root", root)::info )
+//let buildReference () =
+//  CleanDir (output @@ "reference")
+//  for lib in referenceBinaries do
+//    MetadataFormat.Generate
+//      ( bin @@ lib, output @@ "reference", layoutRoots, 
+//        parameters = ("root", root)::info )
 
 // Build documentation from `fsx` and `md` files in `docs/content`
 let buildDocumentation () =
